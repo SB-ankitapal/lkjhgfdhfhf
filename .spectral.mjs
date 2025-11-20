ec31a3126dc720555a25a913733d3012bd106952
ankita.pal@smartbear.com <ankita.pal@smartbear.com>
2025-11-20 08:24:51 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI5OTEwNw/spectral.js?branch_slug=main&token=fef1ff3f-d523-4e6a-9c49-86f2499afe9a";
+export default { extends: ruleset };
